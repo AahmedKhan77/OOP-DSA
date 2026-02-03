c58711caf8d7891eb799a76280129276b35422f8
Aahmed Khan <khanaahmed272@gmail.com>
2026-02-03 22:23:57 +0500

    Update tut1.cpp

--- tut1/tut1.cpp
+++ tut1/tut1.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, C++!" << endl;
+    int a = 10, b = 5;
+    
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+
+    cout << (a > b) << endl;
+    cout << (a == b) << endl;
+
+    cout << (a > 0 && b > 0) << endl;
+    cout << (a > 0 || b < 0) << endl;
+    cout << !a << endl;
+
+    a++;
+    b--;
+
+    cout << (a & b) << endl;
+    cout << (a | b) << endl;
+
+    cout << (a > b ? a : b) << endl;
+
+    cout << sizeof(a) << endl;
+
     return 0;
 }
